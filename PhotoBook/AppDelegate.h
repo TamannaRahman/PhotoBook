df25b1a84669719996c7c52be7a656991dd1ba37
TamannaRahman <tamanna.rahman1988@gmail.com>
2016-07-20 12:08:01 +1000

    Initial Commit

---
+++ PhotoBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoBook
+//
+//  Created by CQUGSR on 20/07/2016.
+//  Copyright © 2016 Tamanna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoBook
+//
+//  Created by CQUGSR on 20/07/2016.
+//  Copyright © 2016 Tamanna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
